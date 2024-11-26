cd2782ebd10b8b752f2954384120a48d617ff0ee
Gene Bogdanovich <genebogdanovich@gmail.com>
2024-11-26 14:50:09 +0300

    Add widget extension and app group.

---
+++ MigrateFromCoreDataToSwiftDataWithAppGroupWidgets/MigrateFromCoreDataToSwiftDataWithAppGroupWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MigrateFromCoreDataToSwiftDataWithAppGroupWidgetsBundle.swift
+//  MigrateFromCoreDataToSwiftDataWithAppGroupWidgets
+//
+//  Created by Gene Bogdanovich on 26.11.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MigrateFromCoreDataToSwiftDataWithAppGroupWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        MigrateFromCoreDataToSwiftDataWithAppGroupWidgets()
+    }
+}
