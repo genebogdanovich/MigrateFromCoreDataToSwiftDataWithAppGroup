2150745c761f1c2450dc740314e2831462190c29
Gene Bogdanovich <genebogdanovich@gmail.com>
2024-11-26 14:48:12 +0300

    Add explicit URL to PersistenceController.

--- MigrateFromCoreDataToSwiftDataWithAppGroup/Persistence.swift
+++ MigrateFromCoreDataToSwiftDataWithAppGroup/Persistence.swift
@@ -7,9 +7,50 @@
 
 import CoreData
 
+// MARK: - PersistenceController
+
 struct PersistenceController {
     static let shared = PersistenceController()
+    
+    private let modelName = "MigrateFromCoreDataToSwiftDataWithAppGroup"
+    
+    private var persistentStoreURL: URL {
+        let applicationSupportDirectoryURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+        return applicationSupportDirectoryURL.appending(path: "\(modelName).sqlite")
+    }
+    
+    
+    
+    let container: NSPersistentContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: modelName)
+        if inMemory {
+            container.persistentStoreDescriptions[0].url = URL(fileURLWithPath: "/dev/null")
+        } else {
+            container.persistentStoreDescriptions[0].url = persistentStoreURL
+        }
+        
+        
+        
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
 
+
+
+
+
+
+// MARK: - Preview
+
+extension PersistenceController {
     @MainActor
     static let preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
@@ -28,30 +69,4 @@ struct PersistenceController {
         }
         return result
     }()
-
-    let container: NSPersistentContainer
-
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "MigrateFromCoreDataToSwiftDataWithAppGroup")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        container.viewContext.automaticallyMergesChangesFromParent = true
-    }
 }
